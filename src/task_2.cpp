688843fd6492c179239150ce75538359514f3c8a
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-11-16 23:49:39 +0500

    Update and rename task_4.h to task_2.cpp

---
+++ src/task_2.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: Asqarjon Anvarov
+ * Date: 16.11.2023
+ * Name: Task 2
+ */
+
+// task_2.cpp
+#include "task_2.h"
+
+void task2_function() {
+   #include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    int length;
+    cout << "Enter the number of coins and bankontes: "; cin >> length;
+    int moneys[length], money_sorted[length];
+    for(int i = 0; i < length; i++){
+        cout << "Enter a denomination: "; cin >> moneys[i];
+    }
+
+    for (int i = 0; i < length; i++){
+        int temp_min = INT_MAX, tempj;
+        for (int j = 0; j < length; j++){
+            if (moneys[j] < temp_min){
+                temp_min = moneys[j];
+                tempj = j;
+            }
+        }
+        money_sorted[i] = temp_min;
+        moneys[tempj] = INT_MAX;
+    }
+    for (int i = 0; i < length; i++){
+        cout << money_sorted[i] << endl;
+    }
+
+    return 0;
+}
+}
