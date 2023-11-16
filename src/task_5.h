b85bfc0b5f5b01d95476343f2ad3343be927c7f3
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-11-16 23:55:04 +0500

    Create task_5.h

---
+++ src/task_5.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Asqarjon Anvarov
+ * Date: 16.11.2023
+ * Name: Task 5
+ */
+// task_5.h
+#ifndef TASK_5_H
+#define TASK_5_H
+
+#include <iostream>
+#include <windows.h>
+
+void task5_function();
+
+#endif // TASK_5_H
