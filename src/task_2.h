f8558bbb4fddb203997610e778529905e6c37478
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-11-16 23:48:51 +0500

    Update and rename task_3.h to task_2.h

---
+++ src/task_2.h
@@ -0,0 +1,17 @@
+/*
+ * Author: Asqarjon Anvarov
+ * Date: 16.11.2023
+ * Name: Task 2
+ */
+// task_2.h
+#ifndef TASK_2_H
+#define TASK_2_H
+
+#include <iostream>
+#include <string>
+#include <climits>
+
+void task2_function();
+
+#endif // TASK_2_H
+
