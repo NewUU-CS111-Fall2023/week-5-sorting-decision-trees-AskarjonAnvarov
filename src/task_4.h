24cfb97a7113aa195fcfea142051f9af3254fc3f
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-11-16 23:53:42 +0500

    Create task_4.h

---
+++ src/task_4.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Asqarjon Anvarov
+ * Date: 16.11.2023
+ * Name: Task 3
+ */
+// task_4.h
+#ifndef TASK_4_H
+#define TASK_4_H
+
+#include <iostream>
+#include <vector>
+
+int countElementsMeetingCriteria(const std::vector<int>& arr);
+
+#endif // TASK_4_H
